65aab3eadc36ddfe57f02214b8db96c274561348
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2025-05-18 09:16:37 -0300

    Cria primeira função

---
+++ Main.hs
@@ -0,0 +1,20 @@
+{-
+* Linguagem Puramente Funcional
+    - Lambda abstração
+    + \x.x
+    + \x.\y.(x y)
+-}
+
+-- PARA RODAR:
+    -- ghci Main.hs
+    -- para imprimir no terminal: idx 1 
+        -- imprimirá 1
+idx = \x -> x
+
+{-- *** Exemplos *** --}
+--      name = expr
+
+-- 1) Calcular o dobro de um número
+
+-- 2) Somar dois inteiros
+
