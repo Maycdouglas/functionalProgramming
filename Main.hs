c36c45486faccf9b46a7f9d0df619ea1b4098a84
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2025-05-24 10:26:52 -0300

    Insere novas funcoes de listas

--- Main.hs
+++ Main.hs
@@ -51,4 +51,75 @@ fat n
 --     - Listas       | - Tuplas
 --     - Funções
 
+-- ***LISTAS***
+-- São tipos de dados HOMOGÊNEOS
+
+-- Construção de lista com operador cons, :
+-- 5:[1,2,3,4,5]                  'A': " small cat"
+
+-- Definição de lista usando intervalos
+-- [1..20]                         ['a'..'z']                    [2,4..20]
+-- [10,9..1]                       [3,6..2*12]                   [3,6,..]
+
+-- Funções já definidas
+
+-- Concat - (++) :: [a] -> [a] -> [a]
+-- ex terminal: (++) [1..3] [4..7]
+meuconcat [] ys = ys -- Se a lista é vazia
+meuconcat (x:xs) ys = x:(meuconcat xs ys)
+-- Em (x:xs), o x é a cabeça e o xs é a cauda da lista (primeiro elemento e o restante da lista)
+
+
+-- Index - (!!) :: [a] -> Int -> a
+-- ex terminal: [1..10] !! 4 - retorna 5
+meuindex (x:xs) 0 = x -- se a lista não é vazia e quero saber o primeiro elemento dela
+meuindex (x:xs) n = meuindex xs (n-1)
+
+
+
+-- length :: [a] -> Int
+-- ex terminal: length [1..5] - retorna 5
+meuLength [] = 0 -- se a lista é vazia
+meuLength (x:xs) = 1 + meuLength xs
+
+
+-- head :: [a] -> a
+-- ex terminal: head [1..5]
+meuHead (x:xs) = x
+
+-- tail :: [a] -> [a]
+-- ex terminal: tail [1..5]
+meuTail (x:xs) = xs
+
+-- reverse :: [a] -> [a]
+-- ex terminal: reverse [1..5]
+meuReverse [] = []
+meuReverse (x:xs) = (meuReverse xs) ++ [x]
+
+-- take :: Int -> [a] -> [a]
+-- captura dos primeiros elementos da lista
+-- ex terminal: take[1..5]
+meuTake 0 xs = [] -- primeiro elemento de uma lista qualquer
+meuTake n [] = [] -- elemento de uma lista vazia
+meuTake n (x:xs) = x:(meuTake (n-1) xs)
+
+
+-- drop :: Int -> [a] -> [a]
+-- ex terminal: drop[1..5]
+meuDrop 0 xs = xs
+meuDrop n [] = []
+meuDrop n (x:xs) = meuDrop (n-1) xs
+
+-- zip :: [a] -> [b] -> [(a,b)]
+-- associa os elementos de uma lista ao elementos de outra de acordo com o indice
+-- ex terminal: zip [1..5] ['a'..'g']
+meuZip [] _ = [] -- o _ significa que o parametro não será referenciado. poderia usar o xs ali também ou qualquer outro nome
+meuZip _ [] = []
+meuZip (x:xs) (y:ys) = (x,y):(meuZip xs ys)
+
+--
+
+--
+
+--
 
